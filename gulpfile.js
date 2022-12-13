ebc4a3aade0d5efeba506166cbe66bccda52c1a1
Eugene Nazirov <jagzonmusic@gmail.com>
2022-12-13 10:25:59 +0600

    Set up an environment with gulp, jQuery, magnific popup etc

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,32 @@ const gulp = require('gulp');
 const cssmin = require('gulp-cssmin');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
+const minify = require('gulp-minify');
+
 
 function sassToCss() {
-  return gulp.src('./src/styles/main.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(cssmin())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('./public'));
+    return gulp.src('./src/styles/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./public'));
+}
+
+function minifyJS() {
+    return gulp.src('./src/scripts/main.js')
+        .pipe(minify({
+            ext:{
+                src:'.js',
+                min:'.min.js'
+            },
+            noSource: true
+        }))
+        .pipe(gulp.dest('./public'))
 }
 
 exports.sass = sassToCss;
+exports.js = minifyJS;
 
 exports.watch = function () {
-  gulp.watch('./src/styles/*.scss', gulp.series('sass'));
+    gulp.watch(['./src/styles/*.scss', './src/scripts/*.js'], gulp.parallel('sass', 'js'));
 };
