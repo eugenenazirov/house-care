51776ad686f2c220a2bd153a9fd671b97dc5521c
Eugene Nazirov <jagzonmusic@gmail.com>
2022-12-13 09:40:32 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# House Care Agency
+
+Diploma project for practicing HTML + CSS + JS.
+
+Made on ITLogia Frontend Development course.
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const gulp = require('gulp');
+const cssmin = require('gulp-cssmin');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+
+function sassToCss() {
+  return gulp.src('./src/styles/main.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./public'));
+}
+
+exports.sass = sassToCss;
+
+exports.watch = function () {
+  gulp.watch('./src/styles/*.scss', gulp.series('sass'));
+};
