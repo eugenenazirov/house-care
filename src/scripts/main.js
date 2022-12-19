686bf8e1aa88b27ba7d81529ae82839aefb8e234
Eugene Nazirov <jagzonmusic@gmail.com>
2022-12-19 10:26:25 +0600

    Stylize tech section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
-// Do not import jQuery here
+const orderProjectBlocks = () => {
+    const projectImageBlocks = $('.project__image');
+    for (let i = 1; i < projectImageBlocks.length; i += 2) {
+        let imageBlock = $(projectImageBlocks[i]);
+        imageBlock.css('order', '1');
+    }
+}
+
+const scrollToProjects = () => {
+    let projects = document.getElementById('projects');
+    projects.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function main() {
+    orderProjectBlocks();
+    $('#scrollDown').click(scrollToProjects);
+}
+
+$(document).ready(main);
