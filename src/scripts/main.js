5c6b735c4da5d08a5b3cedee43328cca5ed1bb7f
Eugene Nazirov <jagzonmusic@gmail.com>
2023-01-04 16:58:39 +0600

    Add magnific popup for images

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
+const popup = $('.popup');
+const projects = $('.projects__project');
+const seeMore = $('.see-more');
+
+
 const orderProjectBlocks = () => {
     const projectImageBlocks = $('.project__image');
     for (let i = 1; i < projectImageBlocks.length; i += 2) {
@@ -6,14 +11,30 @@ const orderProjectBlocks = () => {
     }
 }
 
+const hideThreeProjects = () => {
+    projects.slice(-3).hide(1000);
+    $('.see-more__text').text('Показать еще три проекта');
+    $('.see-more__arrow').css({
+        transform: 'rotate(180deg)'
+    });
+    seeMore.off().click(showThreeProjects);
+}
+
+const showThreeProjects = () => {
+    projects.slice(-3).show(1000);
+    $('.see-more__text').text('Скрыть последние три проекта');
+    $('.see-more__arrow').css({
+        transform: 'rotate(180deg)'
+    });
+    seeMore.off().click(hideThreeProjects);
+}
+
 const scrollToProjects = () => {
     let projects = document.getElementById('projects');
     projects.scrollIntoView({
         behavior: 'smooth'
     });
 }
-
-const popup = $('.popup');
 
 const openPopup = () => {
     popup.removeClass('closed').hide().fadeIn();
@@ -26,14 +47,23 @@ const hidePopup = () => {
 }
 
 function main() {
+    projects.slice(-3).hide();
     orderProjectBlocks();
     $('#tourBtn').click(openPopup);
     $('#scrollDown').click(scrollToProjects);
     $('#closeBtn').click(hidePopup);
+    seeMore.click(showThreeProjects);
     new Glide('.glide', {
         autoplay: 3000,
         hoverpause: true,
     }).mount();
+    $('.project__image').magnificPopup({
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        },
+        type: 'image'
+    });
 }
 
 $(document).ready(main);
